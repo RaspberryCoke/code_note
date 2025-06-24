b3d68803f61f8a5d678909754839ee65a07c45b2
yiduoliu <3205516076@qq.com>
2025-06-24 11:42:19 +0800

    update

--- templates/二分查找/upper.cpp
+++ templates/二分查找/upper.cpp
@@ -1,14 +1,19 @@
 
-int upper_bound(vector<int> &nums, int target) {
-int l = 0, r = nums.size(), mid;
-while (l < r) {
-mid = l + (r - l) / 2;
-if (nums[mid] > target) {
-r = mid;
-} else {
+int upper_bound(vector<int> &nums, int target)
+{
+    int l = 0, r = nums.size(), mid;
+    while (l < r)
+    {
+        mid = l + (r - l) / 2;
+        if (nums[mid] > target)
+        {
+            r = mid;
+        }
+        else
+        {
 
-l = mid + 1;
-}
-}
-return l;
+            l = mid + 1;
+        }
+    }
+    return l;
 }
